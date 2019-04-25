29e5110706aa904bc8ed8abe94a328e27bc39b89
Roman Galaniuk <romangalanuk@gmail.com>
2019-04-25 23:19:24 +0300

    desctop

--- build/js/main.js
+++ build/js/main.js
@@ -1,6 +1,13 @@
 $(document).ready(function () {
   console.log("Скрипты подъехали")
 
+  $('.partners__slider').slick({
+    slidesToShow: 4,
+    dots: true,
+    arrows: false,
+    infinite: true
+  })
+
   //wow
   $(function () {
     new WOW().init();
